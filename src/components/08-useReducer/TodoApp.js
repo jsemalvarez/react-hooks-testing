25406a967b03e105646cbdf34651926f7a0de520
jsemalvarez <jsemalvarez@gmail.com>
2021-08-26 21:10:46 -0300

    HTML y CSS de AppTodo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './styles.css'
+import { todoReducer } from './todoReducer'
 
 const initialState = [{
     id: new Date().getTime(),
@@ -9,18 +10,56 @@ const initialState = [{
 
 export const TodoApp = () => {
 
-    const [state, dispatch] = useReducer(reducer, initialState )
+    const [ todos , dispatch] = useReducer(todoReducer, initialState )
     return (
         <div>
 
-            <h2>TodoApp</h2>
+            <h2>TodoApp ( { todos.length } )</h2>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Reducer</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group-flush">
+                    {
+                        todos.map( (todo, i) => (
+                            <li 
+                                key={ todo.id }
+                                className="list-group-item"
+                            > 
+                                <p className="text-center"> { i + 1 }. { todo.desc } </p> 
+                                <button
+                                    className="btn btn-danger"
+                                >
+                                    Borrar
+                                </button>
+                            </li>
+                        ))
+                    }
+                    </ul>
+                </div>
+                <div className="col-5">
+
+                        <h4>Agregar TODO</h4>
+                        <hr />
+
+                        <form>
+
+                            <input 
+                                type="text"
+                                name="description"
+                                placeholder="Aprender..."
+                                autoComplete="off"
+                                className="form-control"
+                            />
+
+                            <button
+                                className="btn btn-primary btn-block mt-1"
+                            >
+                                Agregar
+                            </button>
+                        </form>
+                </div>
+            </div>
 
         </div>
     )
