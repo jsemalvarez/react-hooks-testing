a1a13dcce080326bd3a56674fc4b0ac6c3d33ab6
jsemalvarez <jsemalvarez@gmail.com>
2021-08-28 14:06:46 -0300

    TodoApp: done todo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -66,6 +66,14 @@ export const TodoApp = () => {
         dispatch( action )
     }
 
+    const handleToggle = ( todoId ) => {
+
+        dispatch({
+            type:'toggle',
+            payload: todoId
+        })
+    }
+
     return (
         <div>
 
@@ -81,7 +89,12 @@ export const TodoApp = () => {
                                 key={ todo.id }
                                 className="list-group-item"
                             > 
-                                <p className="text-center"> { i + 1 }. { todo.desc } </p> 
+                                <p 
+                                    className={`${ todo.done && 'complete'}`}
+                                    onClick={ () => handleToggle( todo.id ) }
+                                > 
+                                    { i + 1 }. { todo.desc } 
+                                </p> 
                                 <button
                                     className="btn btn-danger"
                                     onClick={ () => handleDelete( todo.id ) }
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -9,6 +9,25 @@ export const todoReducer = ( state = [], action ) => {
         case 'delete':
             return state.filter( todo => todo.id !== action.payload )
 
+        case 'toggle-old': // este codigo se megora en el case 'toggle'
+            return state.map( todo => {
+                if( todo.id === action.payload ){
+                    return{
+                        ...todo,
+                        done: !todo.done
+                    }
+                }else{
+                    return todo
+                }
+            })
+
+        case 'toggle':
+            return state.map( todo => 
+                ( todo.id )
+                    ? { ...todo, done: !todo.done}
+                    : todo
+            )
+
         default:
             return state;
     }
