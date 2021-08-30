17f3946f024dc561fcd38e388c688346c05367e3
jsemalvarez <jsemalvarez@gmail.com>
2021-08-29 23:47:00 -0300

    createContext useContext

--- src/components/09-useContext/HomeScreen.js
+++ src/components/09-useContext/HomeScreen.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export const HomeScreen = () => {
+
+    const { user } = useContext( UserContext )
+
     return (
         <div>
             <h1> HomeScreen </h1>
             <hr />
+            <h2> { user.name } </h2>
         </div>
     )
 }
--- src/components/09-useContext/MainScreen.js
+++ src/components/09-useContext/MainScreen.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainScreen = () => {
+
+    const [user, setUser] = useState({
+
+    })
+
+    /**
+     *  UserContext esta pendiente de los cambios de user y avisa a sus componentes hijos de los mismos
+     *  y se redibuja cada una de las partes afectdas 
+     */
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
+            <AppRouter />
+        </UserContext.Provider>
     )
 }
